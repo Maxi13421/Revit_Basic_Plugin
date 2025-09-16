e13367efafbd9032e9e1a8ab177e233b20058d61
Maximilian Gleiser <maxigleiser@gmail.com>
2025-09-17 00:39:13 +0200

    Added python server

---
+++ fastapi/main.py
@@ -0,0 +1,13 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Message(BaseModel):
+    msg: str
+
+
+
+@app.post("/")
+async def create_message(message: Message):
+    return f"Received your message: {message.msg}"
